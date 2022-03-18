90ed7bfa986aaead359e0b3df340358ee4c5e4d5
NabeelHaris <nabeelharis95@gmail.com>
2022-03-18 20:54:03 +0500

    Set functionality of add-to-cart

--- app/components/card/CardComponent.js
+++ app/components/card/CardComponent.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 import {
   ScrollView,
   Image,
@@ -7,26 +7,34 @@ import {
   Text,
   View,
   TouchableOpacity,
-} from 'react-native';
-import {Card, Button, Icon} from 'react-native-elements';
-import {addProductToCart} from '../../apis/CartApis';
-import {setCartProduct} from '../redux/actions/CartAction';
-import {useDispatch} from 'react-redux';
+} from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import { addProductToCart } from "../../apis/CartApis";
+import { setCartProduct } from "../redux/actions/CartAction";
+import { useDispatch, useSelector } from "react-redux";
 
-const CardComponent = props => {
+const CardComponent = (props) => {
   // props
   const navigation = useNavigation();
+  // const product = useSelector((state) => state.cartProducts);
+  // console.log("product:", product);
   const dispatch = useDispatch();
   const [cartData, setCartData] = useState([]);
+  const [count, setCount] = useState(0);
 
   function setProductToCart(item) {
-    console.log('item:', item);
-    setCartData(prev => [...prev, item]);
-    const dataForStore = [...cartData, item];
-
-    dispatch(setCartProduct(dataForStore));
-    //  addProductToCart(item)
-    // localStorage.setItem('cartItem:', JSON.stringify(item));
+    console.log("item:", item);
+    const cartRecord = cartData.find((ele) => ele.id == item.id);
+    if (cartRecord) {
+      alert("Already exist");
+    } else {
+      setCartData((prev) => [...prev, item]);
+      const dataForStore = [...cartData, item];
+      dispatch(setCartProduct(dataForStore));
+      console.log("dataForStore:", dataForStore);
+      setCount(count + 1);
+    }
+  
   }
 
   return (
@@ -34,15 +42,18 @@ const CardComponent = props => {
       <View>
         <TouchableOpacity
           activeOpacity={0.5}
-          onPress={() => navigation.navigate('Cart')}
-          style={styles.baskit}>
+          onPress={() => navigation.navigate("Cart")}
+          style={styles.baskit}
+        >
           <Image
             source={{
-              uri: 'https://p.kindpng.com/picc/s/714-7147174_png-file-svg-transparent-background-shopping-cart-icon.png',
+              uri: "https://p.kindpng.com/picc/s/714-7147174_png-file-svg-transparent-background-shopping-cart-icon.png",
             }}
             style={styles.tinyLogo}
           />
-          <Text style={styles.counterTextstyl}>0</Text>
+          <Text style={styles.counterTextstyl}>
+            {cartData && cartData.length > 0 ? count : 0}
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
@@ -51,7 +62,7 @@ const CardComponent = props => {
             <Card key={i}>
               <Image
                 resizeMode="cover"
-                source={{uri: list.image}}
+                source={{ uri: list.image }}
                 style={styles.img}
               />
               <Card.Title style={styles.titlesty}>{list.title}</Card.Title>
@@ -62,7 +73,7 @@ const CardComponent = props => {
                   title="Product Details"
                   style={styles.btnSty}
                   onPress={() =>
-                    navigation.navigate('ProductDetail', {
+                    navigation.navigate("ProductDetail", {
                       itemId: list.id,
                     })
                   }
@@ -95,33 +106,33 @@ const styles = StyleSheet.create({
   img: {
     width: 120,
     height: 200,
-    marginRight: 'auto',
-    marginLeft: 'auto',
+    marginRight: "auto",
+    marginLeft: "auto",
     marginBottom: 20,
   },
   titlesty: {
-    color: 'black',
+    color: "black",
     fontSize: 20,
     // fontWeight: 500,
     opacity: 1,
   },
   pricesty: {
-    color: 'black',
+    color: "black",
     fontSize: 30,
     // fontWeight: 600,
   },
   priceSty: {
-    color: 'black',
+    color: "black",
     fontSize: 20,
-    fontWeight: 'bold',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    fontWeight: "bold",
+    marginLeft: "auto",
+    marginRight: "auto",
     marginBottom: 10,
   },
   btnsStyl: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   btnSty: {
     width: 200,
@@ -130,22 +141,22 @@ const styles = StyleSheet.create({
     borderRadius: 30,
   },
   baskit: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   counterTextstyl: {
-    position: 'relative',
+    position: "relative",
     right: 15,
-    textAlign: 'right',
+    textAlign: "right",
     marginRight: 5,
     fontSize: 20,
-    fontWeight: '700',
-    color: 'blue',
+    fontWeight: "700",
+    color: "blue",
   },
   tinyLogo: {
-    alignItems: 'flex-end',
-    marginLeft: 'auto',
+    alignItems: "flex-end",
+    marginLeft: "auto",
     marginRight: 10,
     marginTop: 15,
     marginBottom: 0,
--- app/components/cart/Cart.js
+++ app/components/cart/Cart.js
@@ -1,36 +1,86 @@
-import React, {useState, useEffect} from 'react';
-import {ScrollView, Image, StyleSheet, Text, View} from 'react-native';
-import {Card, Button, Icon} from 'react-native-elements';
-import {useDispatch, useSelector} from 'react-redux';
-import {getAllCartproducts} from '../../apis/CartApis';
-import { setCartProduct } from '../redux/actions/CartAction';
-import {connect} from 'react-redux';
-const Cart = props => {
-  const product = useSelector(state => state.cartProducts);
-  console.log('cart-product:', product);
+import React, { useState, useEffect } from "react";
+import { ScrollView, Image, StyleSheet, Text, View } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCartproducts } from "../../apis/CartApis";
+import { setCartProduct } from "../redux/actions/CartAction";
+import { connect } from "react-redux";
+const Cart = (props) => {
+  const product = useSelector((state) => state.cartProducts);
+  let [totalPrice, setTotalPrice] = useState(0);
+  const [quantityCount, setQuantity] = useState(1);
+  console.log("cart-product:", product);
 
+  function incQuantity(item) {
+    setQuantity(quantityCount + 1);
+    totalPrice += item.price;
+    setTotalPrice(totalPrice);
+  }
+  function decQuantity(item) {
+    if (quantityCount >= 1) {
+      setQuantity(quantityCount - 1);
+      totalPrice -= item.price;
+      setTotalPrice(totalPrice);
+    }
+    else {
+      setTotalPrice(0)
+    }
+  }
+  function getTotalAmount() {
+    let price = 0;
+    product.productList &&
+      product.productList.length > 0 &&
+      product.productList.map((list, i) => {
+        console.log("list--->", list);
+        price += list.price;
+      });
+    setTotalPrice(price);
+  }
   useEffect(() => {
-    // props.getAllCartproducts();
-    // return () => {
-    //   cleanup
-    // };
-    setCartProduct
+    getTotalAmount();
+    // setCartProduct;
   }, []);
   return (
     <View>
-      <Text>Cart Component</Text>
-      {/* {props.productList?.productList?.products?.map((list, i) => (
-        <ScrollView key={i}>
-          <Card >
-            <Text style={styles.priceSty}>Quantity: {list.quantity}</Text>
-            <Card.Divider />
-          </Card>
-        </ScrollView>
-      ))} */}
+      <ScrollView>
+        <Text style={styles.cartTitle}>Cart Component</Text>
+        {product.productList?.map((item, i) => (
+          <View style={styles.card} key={i}>
+            <View style={styles.info}>
+              <Image
+                resizeMode="cover"
+                source={{ uri: item.image }}
+                style={styles.img}
+              />
+
+              <View>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <View style={styles.counter}>
+                  <Text style={styles.incBtn} onPress={() => incQuantity(item)}>
+                    +
+                  </Text>
+                  <Text style={styles.quantity}>{quantityCount}</Text>
+                  <Text style={styles.incBtn} onPress={() => decQuantity(item)}>
+                    -
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.priceSty}>
+              <Text style={styles.priceTextSty}>Rs/. {item.price} $</Text>
+            </View>
+          </View>
+        ))}
+        <View style={styles.total}>
+          <Text style={styles.totalText}>
+            Total: {totalPrice}
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     productList: state.cartProducts,
   };
@@ -38,75 +88,97 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
   // getAllCartproducts,
-  setCartProduct
+  setCartProduct,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
 
 const styles = StyleSheet.create({
-  cardsty: {
+  cartTitle: {
+    color: "#58a6ff",
+    fontWeight: "800",
     fontSize: 25,
-    marginBottom: 10,
-    // fontWeight: 500,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    width: 350,
+    height: "auto",
+    marginBottom: 30,
+    marginTop: "auto",
+    marginLeft: "auto",
+    marginRight: 20,
+    borderRadius: 20,
+  },
+  info: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  titleText: {
+    width: 245,
+    marginTop: 10,
+    fontWeight: "600",
+    color: "black",
   },
   img: {
-    width: 120,
-    height: 200,
-    marginRight: 'auto',
-    marginLeft: 'auto',
-    marginBottom: 20,
-  },
-  titlesty: {
-    color: 'black',
-    fontSize: 20,
-    // fontWeight: 500,
-    opacity: 1,
-  },
-  pricesty: {
-    color: 'black',
-    fontSize: 30,
-    // fontWeight: 600,
+    width: 100,
+    height: 100,
+    borderRadius: 30,
+    marginRight: "auto",
+    marginLeft: 10,
+    marginTop: "auto",
+    marginBottom: "auto",
+    aspectRatio: 1,
+    resizeMode: "contain",
   },
   priceSty: {
-    color: 'black',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    marginRight: "auto",
+    marginLeft: 35,
+    marginTop: 15,
+  },
+  priceTextSty: {
+    fontWeight: "700",
+    color: "#58a6ff",
     marginBottom: 10,
   },
-  btnsStyl: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  btnSty: {
-    width: 200,
-    marginHorizontal: 50,
-    marginVertical: 10,
-    borderRadius: 30,
-  },
-  baskit: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  counterTextstyl: {
-    position: 'relative',
-    right: 15,
-    textAlign: 'right',
-    marginRight: 5,
-    fontSize: 20,
-    fontWeight: '700',
-    color: 'blue',
-  },
-  tinyLogo: {
-    alignItems: 'flex-end',
-    marginLeft: 'auto',
-    marginRight: 10,
+  counter: {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: "auto",
+    marginRight: "auto",
     marginTop: 15,
-    marginBottom: 0,
-    width: 30,
-    height: 30,
+  },
+  incBtn: {
+    color: "white",
+    backgroundColor: "#58a6ff",
+    borderRadius: 50,
+    width: 26,
+    height: 28,
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  quantity: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "600",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  total: {
+    alignItems: "flex-end",
+    marginRight: 25,
+    marginBottom: 20,
+  },
+  totalText: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "700",
   },
 });
